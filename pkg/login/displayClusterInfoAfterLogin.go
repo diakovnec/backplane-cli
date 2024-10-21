a32b08fd4e5417b260bc7c9fee61f4d189191963
ydiakov <diakovnec@gmail.com>
2024-10-21 11:26:23 +0800

    adding new function

--- pkg/login/displayClusterInfoAfterLogin.go
+++ pkg/login/displayClusterInfoAfterLogin.go
@@ -1,72 +1,34 @@
 package login
 
 import (
-        "context"
-        "errors"
-        "fmt"
-        "net"
-        "net/http"
-        "net/url"
-        "os"
-        "regexp"
-        "strings"
+	"fmt"
 
-        "github.com/golang-jwt/jwt/v4"
-        logger "github.com/sirupsen/logrus"
-        "github.com/spf13/cobra"
-        "k8s.io/cli-runtime/pkg/genericclioptions"
-        "k8s.io/client-go/rest"
-        "k8s.io/client-go/tools/clientcmd/api"
+	logger "github.com/sirupsen/logrus"
+	"k8s.io/client-go/rest"
 
-        BackplaneApi "github.com/openshift/backplane-api/pkg/client"
-
-        ocmsdk "github.com/openshift-online/ocm-sdk-go"
-        "github.com/openshift/backplane-cli/pkg/backplaneapi"
-        "github.com/openshift/backplane-cli/pkg/cli/config"
-        "github.com/openshift/backplane-cli/pkg/cli/globalflags"
-        "github.com/openshift/backplane-cli/pkg/jira"
-        "github.com/openshift/backplane-cli/pkg/login"
-        "github.com/openshift/backplane-cli/pkg/ocm"
-        "github.com/openshift/backplane-cli/pkg/pagerduty"
-        "github.com/openshift/backplane-cli/pkg/utils"
-
+	ocmsdk "github.com/openshift-online/ocm-sdk-go"
+	"github.com/openshift/backplane-cli/pkg/cli/config"
+	"github.com/openshift/backplane-cli/pkg/ocm"
 )
-
 
 // GetRestConfig returns a client-go *rest.Config which can be used to programmatically interact with the
 // Kubernetes API of a provided clusterID
-func displyIfClusterAccessProtectionEnabled(ocmConnection *ocmsdk.Connection, clusterID string) (*rest.Config, error) {
-	cluster, err := ocm.DefaultOCMInterface.IsClusterAccessProtectionEnabled(ocmConnection, clusterID)
+func ConnectToTheCluster(bp config.BackplaneConfiguration, ocmConnection *ocmsdk.Connection, clusterID string) (*rest.Config, error) {
+	cluster, err := ocm.DefaultOCMInterface.GetClusterInfoByIDWithConn(ocmConnection, clusterID)
 	if err != nil {
-			return nil, err 
+		return nil, err
 	}
 
 	accessToken, err := ocm.DefaultOCMInterface.GetOCMAccessTokenWithConn(ocmConnection)
 	if err != nil {
-			return nil, err
+		return nil, err
 	}
 
-	bpAPIClusterURL, err := doLogin(bp.URL, clusterID, *accessToken)
-	if err != nil {
-			return nil, fmt.Errorf("failed to backplane login to cluster %s: %v", cluster.Name(), err)
-	}
-
-	cfg := &rest.Config{
-			Host:        bpAPIClusterURL,
-			BearerToken: *accessToken,
-	}
-
-	if bp.ProxyURL != nil {
-			cfg.Proxy = func(*http.Request) (*url.URL, error) {
-					return url.Parse(*bp.ProxyURL)
-			}
-	}
-
-	return cfg, nil
+	return nil, nil
 }
 
-
-
+//////////////////////////////////////////////////////////////////
+//////////// this is working code ////////////////////////////////
 
 // displayClusterInfo retrieves and displays basic information about the target cluster.
 func displayClusterInfo(clusterID string) error {
@@ -75,7 +37,7 @@ func displayClusterInfo(clusterID string) error {
 	// Retrieve cluster information
 	clusterInfo, err := ocm.DefaultOCMInterface.GetClusterInfoByID(clusterID)
 	if err != nil {
-			return fmt.Errorf("error retrieving cluster info: %v", err)
+		return fmt.Errorf("error retrieving cluster info: %v", err)
 	}
 
 	// Display cluster information
@@ -87,11 +49,14 @@ func displayClusterInfo(clusterID string) error {
 
 	logger.Info("Basic cluster information displayed.")
 	return nil
-}	
+}
 
 // to call the current function
-	if err = displayClusterInfo(clusterID); err != nil {
+func calldisplayClusterInfo(clusterID string) error {
+	if err := displayClusterInfo(clusterID); err != nil {
 		return err
 	}
+	return nil
+}
 
-
+//////////////////////////////////////////////////////////////////
